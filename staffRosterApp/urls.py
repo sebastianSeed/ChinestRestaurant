b4220ffc809bc83144a6f52e425f96618855b43a
sebastian <test@test.com>
2013-06-26 17:38:12 +1000

    Updating with current progress before openshift pull

--- staffRosterApp/urls.py
+++ staffRosterApp/urls.py
@@ -4,6 +4,8 @@ from staffRosterApp import views
 
 urlpatterns = patterns('',
     url(r'^view', views.rosterView ,name='roster'),
+    url(r'^updateRoster', views.updateDB,name='updateRoster'),
+    
 
     
     
--- staffRosterApp/views.py
+++ staffRosterApp/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import request
+from staffRosterApp.models import CalendarShift, Employee
+from django.contrib.auth.models import User
+
+from django.utils import simplejson
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 #This function checks if the user is logged in 
@@ -22,6 +29,7 @@ def rosterView(request):
 
 def staffRosterView(request):
     template = loader.get_template('staffRosterApp/staffRoster.html')
+    
     context = RequestContext(request, {
         'viewType': "dev - staff view!!",
     })
@@ -29,8 +37,26 @@ def staffRosterView(request):
 
 
 def adminRosterView(request):
-    template = loader.get_template('staffRosterApp/staffRoster.html')
+    template = loader.get_template('staffRosterApp/staffRoster.html')   
+    events   = CalendarShift.objects.all().values()
+
     context = RequestContext(request, {
-        'viewType': "dev - admin view!!",
+        'eventsDictionary' : events,
     })
     return HttpResponse(template.render(context))
+
+
+
+
+def updateDB(request):
+    userObj     = User.objects.all().get(username__iexact= request.POST["staffName"])
+    newShift = CalendarShift(title   = userObj, 
+                             allDay      = request.POST["allDay"],
+                             start      = request.POST["start"],
+                             end        = request.POST["end"]
+                                    )
+    newShift.save()  
+
+    #Return blank status OK response
+    return HttpResponse()
+        
